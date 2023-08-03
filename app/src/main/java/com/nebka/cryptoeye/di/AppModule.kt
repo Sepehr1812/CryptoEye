6684e7eee5c7992fe63a3a3766ab5b677d1e3ea6
Sepehr1812 <sepehrakhoundi1812@gmail.com>
2023-08-04 02:03:33 +0330

    add db elements to app module.

--- app/src/main/java/com/nebka/cryptoeye/di/AppModule.kt
+++ app/src/main/java/com/nebka/cryptoeye/di/AppModule.kt
@@ -1,11 +1,16 @@
 package com.nebka.cryptoeye.di
 
+import android.content.Context
+import androidx.room.Room
 import com.nebka.cryptoeye.BuildConfig
+import com.nebka.cryptoeye.data.db.CryptoEyeDatabase
+import com.nebka.cryptoeye.data.remote.api.TagApi
 import com.nebka.cryptoeye.util.Constants.AUTHENTICATION_TOKEN
 import com.nebka.cryptoeye.util.Constants.BASE_URL
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
@@ -20,32 +25,30 @@ object AppModule {
 
     @Singleton
     @Provides
-    fun provideOkHttpClient(): OkHttpClient {
-        // add authentication token as header field of the requests
-        val requestInterceptor = Interceptor { chain ->
-            val newRequest = chain.request().newBuilder()
-                .addHeader("Authorization", AUTHENTICATION_TOKEN)
-                .build()
-            chain.proceed(newRequest)
-        }
-
-        if (BuildConfig.DEBUG) {
-            val loggingInterceptor = HttpLoggingInterceptor().apply {
-                setLevel(HttpLoggingInterceptor.Level.HEADERS)
-                setLevel(HttpLoggingInterceptor.Level.BODY)
-            }
-
-            return OkHttpClient
-                .Builder()
-                .addInterceptor(requestInterceptor)
-                .addInterceptor(loggingInterceptor)
-                .build()
-
-        } else return OkHttpClient
-            .Builder()
-            .addInterceptor(requestInterceptor)
-            .build()
+    fun provideHttpLoggingInterceptor() = HttpLoggingInterceptor().apply {
+        setLevel(HttpLoggingInterceptor.Level.HEADERS)
+        setLevel(HttpLoggingInterceptor.Level.BODY)
     }
+
+    @Singleton
+    @Provides
+    fun provideRequestInterceptor() = Interceptor { chain ->
+        val newRequest = chain.request().newBuilder()
+            .addHeader("Authorization", AUTHENTICATION_TOKEN)
+            .build()
+        chain.proceed(newRequest)
+    }
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(
+        requestInterceptor: Interceptor,
+        httpLoggingInterceptor: HttpLoggingInterceptor
+    ) = OkHttpClient.Builder()
+        .addInterceptor(requestInterceptor).apply {
+            if (BuildConfig.DEBUG) addInterceptor(httpLoggingInterceptor)
+        }
+        .build()
 
     @Provides
     @Singleton
@@ -55,4 +58,23 @@ object AppModule {
             .client(client)
             .addConverterFactory(MoshiConverterFactory.create())
             .build()
+
+    @Singleton
+    @Provides
+    fun provideDatabase(@ApplicationContext context: Context) =
+        synchronized(CryptoEyeDatabase::class) {
+            Room.databaseBuilder(
+                context,
+                CryptoEyeDatabase::class.java,
+                "CryptoEyeDatabase"
+            ).build()
+        }
+
+    @Singleton
+    @Provides
+    fun provideTagApi(retrofit: Retrofit): TagApi = retrofit.create(TagApi::class.java)
+
+    @Singleton
+    @Provides
+    fun provideTagDao(cryptoEyeDatabase: CryptoEyeDatabase) = cryptoEyeDatabase.tagDao()
 }
