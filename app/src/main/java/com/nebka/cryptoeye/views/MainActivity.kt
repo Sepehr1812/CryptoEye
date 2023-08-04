805025c3cfc21c0d290db8390373bfb7b9e316bd
Sepehr1812 <sepehrakhoundi1812@gmail.com>
2023-08-05 02:47:35 +0330

    add SplashScreen.

--- app/src/main/java/com/nebka/cryptoeye/views/MainActivity.kt
+++ app/src/main/java/com/nebka/cryptoeye/views/MainActivity.kt
@@ -6,13 +6,20 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.nebka.cryptoeye.ui.theme.CryptoEyeTheme
+import com.nebka.cryptoeye.util.Constants.SEARCH_SCREEN
+import com.nebka.cryptoeye.util.Constants.SPLASH_SCREEN
+import dagger.hilt.android.AndroidEntryPoint
 
+
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -23,7 +30,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Navigation()
                 }
             }
         }
@@ -31,17 +38,20 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun Navigation() {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CryptoEyeTheme {
-        Greeting("Android")
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = SPLASH_SCREEN
+    ) {
+        composable(SPLASH_SCREEN) {
+            SplashScreen(navController = navController)
+        }
+
+        composable(SEARCH_SCREEN) {
+            SearchScreen()
+        }
     }
 }
---
+++ app/src/main/java/com/nebka/cryptoeye/views/SearchScreen.kt
@@ -0,0 +1,9 @@
+package com.nebka.cryptoeye.views
+
+import androidx.compose.runtime.Composable
+
+
+@Composable
+fun SearchScreen() {
+
+}
